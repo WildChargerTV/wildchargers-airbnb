3f34b4062be987da79d187c1012961a5b5cbefb3
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-07-24 20:34:51 -0700

    Added login validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,11 +4,25 @@ const { Op } = require('sequelize');
 const bcrypt = require('bcryptjs');
 const router = express.Router();
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+// Validate a login (NOT an endpoint)
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
+
 // Log in
-router.post('/', async (req, res, next) => {
+router.post('/', validateLogin, async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.unscoped().findOne({
