8dbbcc819d3f89af71c0c7cfa4eccb7db4feea98
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-07-24 20:17:16 -0700

    Added User login

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,7 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
 const { User } = require('../../db/models');
 const { restoreUser } = require('../../utils/auth.js');
@@ -8,5 +10,13 @@ const { restoreUser } = require('../../utils/auth.js');
     // If current user session is valid, set req.user to the user in the database
     // If current user session is not valid, set req.user to null
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
