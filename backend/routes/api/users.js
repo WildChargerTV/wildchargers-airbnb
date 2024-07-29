c2794f55ea1fee55c85e6dbfc5153a35a34df066
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-07-29 06:02:12 -0700

    firstName and lastName are now properly included in validation checks and User creation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -14,28 +14,35 @@ const validateSignup = [
     check('email')
       .exists({ checkFalsy: true })
       .isEmail()
-      .withMessage('Please provide a valid email.'),
+      .withMessage('Invalid email'),
     check('username')
       .exists({ checkFalsy: true })
       .isLength({ min: 4 })
-      .withMessage('Please provide a username with at least 4 characters.'),
+      .withMessage('Username is required'),
     check('username')
       .not()
       .isEmail()
-      .withMessage('Username cannot be an email.'),
+      .withMessage('Username cannot be an email'),
     check('password')
       .exists({ checkFalsy: true })
       .isLength({ min: 6 })
-      .withMessage('Password must be 6 characters or more.'),
+      .withMessage('Password must be 6 characters or more'),
+    check('firstName')
+      .exists({ checkFalsy: true })
+      .withMessage('First Name is required'),
+    check('lastName')
+      .exists({ checkFalsy: true })
+      .withMessage('Last Name is required'),
     handleValidationErrors
-  ];
+];
 
 // Sign up
 router.post('/', validateSignup, async (req, res) => {
-    const { email, password, username } = req.body;
+    const { email, password, username, firstName, lastName } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
-    const user = await User.create({ email, username, hashedPassword });
 
+    const user = await User.create({ email, username, hashedPassword, firstName, lastName }); 
+    
     const safeUser = {
       id: user.id,
       email: user.email,
