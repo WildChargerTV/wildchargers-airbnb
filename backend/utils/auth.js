2164ce69dc844006d8dd0ab33c2c157b9e585132
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-08-02 07:50:19 -0700

    Time-saving console.log added for this method (most likely temporary)

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -63,13 +63,14 @@ const restoreUser = (req, res, next) => {
 
 // If there is no current user, return an error
 const requireAuth = function (req, _res, next) {
-    if (req.user) return next();
-  
-    const err = new Error('Authentication required');
-    err.title = 'Authentication required';
-    err.errors = { message: 'Authentication required' };
-    err.status = 401;
-    return next(err);
+  console.log('\u001b[1;33mUser authentication requested');
+  if (req.user) return next();
+
+  const err = new Error('Authentication required');
+  err.title = 'Authentication required';
+  err.errors = { message: 'Authentication required' };
+  err.status = 401;
+  return next(err);
 };
 
 module.exports = { setTokenCookie, restoreUser, requireAuth };
