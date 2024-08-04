63b4d40dbcdc1a372449d7033213a706947586e7
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-08-04 06:58:50 -0700

    Removed validators (they're centralized now)

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,9 +1,9 @@
 // backend/routes/api/spots.js
 const express = require('express')
 
-const { check } = require('express-validator');
-const { requireAuthentication, requireAuthorization } = require('../../utils/auth')
-const { handleValidationErrors } = require('../../utils/validation');
+const { requireAuthentication, requireAuthorization } = require('../../utils/auth');
+const { findInstance } = require('../../utils/search');
+const { validateSpot, validateImage, validateReview, validateBooking } = require('../../utils/validation');
 require('dotenv').config();
 require('express-async-errors');
 
@@ -11,131 +11,6 @@ const { User, Spot, Image, Sequelize, Review, Booking } = require('../../db/mode
 const { Op } = require('sequelize');
 
 const router = express.Router();
-
-// Validate a new Spot
-const validateSpot = [
-    check('address')
-        .exists({ checkFalsy: true })
-        .withMessage('Street address is required'),
-    check('city')
-        .exists({ checkFalsy: true })
-        .withMessage('City is required'),
-    check('state')
-        .exists({ checkFalsy: true })
-        .withMessage('State is required'),
-    check('country')
-        .exists({ checkFalsy: true })
-        .withMessage('Country is required'),
-    check('lat')
-        .exists({ checkFalsy: true })
-        .isFloat({ min: -90, max: 90 })
-        .withMessage('Latitude must be within -90 and 90'),
-    check('lng')
-        .exists({ checkFalsy: true })
-        .isFloat({ min: -180, max: 180 })
-        .withMessage('Longitude must be within -180 and 180'),
-    check('name')
-        .exists({ checkFalsy: true }).withMessage('Name must be less than 50 characters') // TODO why is it stopping here?
-        .isLength({ max: 50 })
-        .withMessage('Name must be less than 50 characters'),
-    check('description')
-        .exists({ checkFalsy: true })
-        .withMessage('Description is required'),
-    check('price')
-        .exists({ checkFalsy: true })
-        .isInt({ min: 1 })
-        .withMessage('Price per day must be a positive number'),
-    handleValidationErrors
-];
-
-// Validate a new Image
-const validateImage = [
-    check('url')
-        .exists({ checkFalsy: true })
-        .isURL()
-        .withMessage('URL does not exist or is invalid'),
-    check('preview')
-        .isBoolean()
-        .withMessage('Value passed into preview was not a boolean'),
-    handleValidationErrors
-];
-
-// Validate a new Review
-const validateReview = [
-    check('review')
-        .exists({ checkFalsy: true })
-        .withMessage('Review text is required'),
-    check('stars')
-        .exists({ checkFalsy: true })
-        .isInt({ min: 1, max: 5 })
-        .withMessage('Stars must be an integer from 1 to 5'),
-    handleValidationErrors
-];
-
-// Validate a new Booking
-const validateBooking = async (req, _res, next) => {
-    let { startDate, endDate } = req.body;
-    const errors = {};
-
-    // Translate the start & end dates to Date objects. Discard hour differentials from Date.now().
-    // This allows for proper comparison if booking dates are the same.
-    startDate = new Date(startDate).setHours(0, 0, 0, 0);
-    endDate = new Date(endDate).setHours(0, 0, 0, 0);
-    let nowDate = new Date(Date.now()).setHours(0, 0, 0, 0);
-
-    // BEGIN Body Validation Errors
-    // 1. startDate cannot be in the past
-    if(startDate < nowDate) errors['startDate'] = 'startDate cannot be in the past';
-    //2. endDate cannot be on or before startDate
-    if(endDate <= startDate) errors['endDate'] = 'endDate cannot be on or before startDate';
-    if(errors.startDate || errors.endDate) {
-        const nextErr = new Error('Bad Request');
-        nextErr.errors = errors;
-        nextErr.status = 400;
-        return next(nextErr);
-    }
-    // END Body Validation Errors
-
-    // BEGIN Booking conflict
-    // TODO port this to be handled by Sequelize unique constraints maybe
-    await Booking.findAll({ where: { spotId: { [Op.eq]: req.body.param } } })
-    .then(async (result) => {
-        for await (const booking of result) {
-            const json = booking.toJSON();
-            let bookingStart = new Date(json.startDate).setHours(0, 0, 0, 0);
-            let bookingEnd = new Date(json.endDate).setHours(0, 0, 0, 0);
-
-            // 1. startDates cannot conflict
-            if(startDate === bookingStart) errors['startDate'] = 'Start date conflicts with an existing booking';
-            // 2. endDates cannot conflict
-            if(endDate === bookingEnd) errors['endDate'] = 'End date conflicts with an existing booking';
-        }
-        return result;
-    });
-    if(errors.startDate || errors.endDate) {
-        const nextErr = new Error('Sorry, this Spot is already booked for the specified dates');
-        nextErr.errors = errors;
-        nextErr.status = 403;
-        return next(nextErr);
-    }
-    // END Booking conflict
-
-    return next();
-}
-
-// Verify the existence of a Spot, if the URL has a spotId
-const findInstance = async (req, res, next) => {
-    const { type, Model, param, options } = req.body;
-    const target = await Model.findByPk(param, options);
-
-    if(!target.toJSON()?.id) { // TODO oddity: some custom columns can cause an object with all null values to be returned rather than no object. Can this be avoided?
-        res.status(404);
-        return res.json({ message: `${type} couldn't be found` });
-    } else {
-        req.body.instance = target;
-        return next();
-    }
-}
 
 // GET all Spots
 router.get('/', async (_req, res) => {
@@ -332,7 +207,7 @@ router.post('/:spotId/images', requireAuthentication, (req, _res, next) => {
 
     if(imgCount >= 10) {
         res.status(403);
-        return res.json({ message: 'Forbidden: Maximum image limit of 10 reached for this Spot '});
+        return res.json({ message: 'Forbidden: Maximum image limit of 10 reached for this Spot'});
     }
 
     let image;
