dd073d322d19b8a35d97f6b556c99054ceef03b2
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-08-04 06:57:47 -0700

    Centralized the instance finder

---
+++ backend/utils/search.js
@@ -0,0 +1,17 @@
+// backend/utils/search.js
+
+// Verify the existence of a Spot, if the URL has a spotId
+const findInstance = async (req, res, next) => {
+    const { type, Model, param, options } = req.body;
+    const target = await Model.findByPk(param, options);
+
+    if(!target.toJSON()?.id) { // TODO oddity: some custom columns can cause an object with all null values to be returned rather than no object. Can this be avoided?
+        res.status(404);
+        return res.json({ message: `${type} couldn't be found` });
+    } else {
+        req.body.instance = target;
+        return next();
+    }
+}
+
+module.exports = { findInstance };
