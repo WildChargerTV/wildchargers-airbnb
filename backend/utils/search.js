7ab235a5ca228e3fa2a3d69d821359a06ffc3c60
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-08-04 23:57:24 -0700

    bugfix: repaired missing optional chain

--- backend/utils/search.js
+++ backend/utils/search.js
@@ -5,7 +5,7 @@ const findInstance = async (req, res, next) => {
     const { type, Model, param, options } = req.body;
     const target = await Model.findByPk(param, options);
 
-    if(!target.toJSON()?.id) { // TODO oddity: some custom columns can cause an object with all null values to be returned rather than no object. Can this be avoided?
+    if(!target?.toJSON()?.id) { // TODO oddity: some custom columns can cause an object with all null values to be returned rather than no object. Can this be avoided?
         res.status(404);
         return res.json({ message: `${type} couldn't be found` });
     } else {
