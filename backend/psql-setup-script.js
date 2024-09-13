cd9784908f0089143845d047b7345cf071a3d084
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-09-13 05:59:30 -0700

    psql setup attempt 5

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -3,13 +3,15 @@ const { sequelize } = require('./db/models');
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if(data.includes(process.env.SCHEMA)) {
+    console.log("checkpoint: schema found, dropping")
     await sequelize.dropSchema(process.env.SCHEMA);
   }
-  console.log("checkpoint 1: schema dropper check done")
-}).then(sequelize.showAllSchemas({ logging: false }))
-.then(async (data) => {
+  
+})
+
+sequelize.showAllSchemas({ logging: false }).then(async (data) => {
   if (!data.includes(process.env.SCHEMA)) {
+    console.log("checkpoint: schema not found, creating")
     await sequelize.createSchema(process.env.SCHEMA);
   }
-  console.log("checkpoint 2: schema maker check done")
 });
