f053a79d8628a221a20b09373cc4b9649f1c3420
WildCharger <46546991+WildChargerTV@users.noreply.github.com>
2024-09-13 05:46:30 -0700

    psql setup now includes a schema drop command

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -2,6 +2,9 @@
 const { sequelize } = require('./db/models');
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
+  if(data.includes(process.env.SCHEMA)) {
+    await sequelize.dropSchema(process.env.SCHEMA);
+  }
   if (!data.includes(process.env.SCHEMA)) {
     await sequelize.createSchema(process.env.SCHEMA);
   }
